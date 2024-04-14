db977a61772bc42bc187f7f100d4f09c7fd46b6a
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-04-14 14:28:03 +0800

    feat(`anchor`): make accounts to interact with in website

--- anchor/programs/solhot/src/lib.rs
+++ anchor/programs/solhot/src/lib.rs
@@ -6,10 +6,46 @@ declare_id!("7hgP3DN1LsuaoHJEJ81iwqcHA6e4tsN2QdS5JFYdDUkn");
 pub mod solhot {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, temperature: u8) -> Result<()> {
+        let record: &mut Account<'_, Record>= &mut ctx.accounts.record;
+        record.temperature = temperature;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+
+// executable account (drainer NFTs :<)
+// practically everything is an account
+// NFT has 3 accounts = wallet account, Associate Token Account (ATA) - PDA, mint account (ID, keypair)
+pub struct Initialize<'info> {
+    #[account(mut)]                             // account METADATA
+    pub payer: Signer<'info>,                   // payer is the one paying for the transaction
+    #[account(                                  // PATTERN (that you'll always use): initialize/create a new account, set who pays, how much space is needed to be allocated
+        init,                               
+        payer = payer,                      
+        space = 8 + Record:: INIT_SPACE
+    )]
+    pub record: Account<'info, Record>,             // account = similar to a table in SQL
+
+    // Note: you can make more accounts under here with
+    // #[account(
+    //     ...
+    // )]
+    // pub someStructInstantiation: Account<'info, Record>,
+
+    pub system_program: Program<'info, System>, // don't have to think about system_program, only needed to create accounts
+                                                //      not necessarily executable even if it has system_program, 
+                                                //      literally system_program is just used for making the account
+}
+
+
+#[account]  // explicitly saying that this is meant for an account
+            // makes the program understand that this struct Record 
+            // is the structure of an account in your program
+#[derive(InitSpace)]    // structure of an account
+                        // dictates how much space you need
+pub struct Record { // account Record = similar to a table in SQL
+    #[max_len(50)]
+    pub temperature: u8,
+}
--- anchor/tests/solhot.ts
+++ anchor/tests/solhot.ts
@@ -6,11 +6,24 @@ describe("solhot", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const provider = anchor.getProvider();
   const program = anchor.workspace.Solhot as Program<Solhot>;
 
   it("Is initialized!", async () => {
+    const postKeypair = anchor.web3.Keypair.generate();
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+    .initialize(1) // the message that's going to be put in the post account
+    .accounts({ 
+      payer: provider.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      record: postKeypair.publicKey,
+    })
+    .signers([postKeypair])  // synonymous to payers
+                  // when instantiating provider, your wallets are already in that provider (Phantom Wallet)
+                  // IMPORTANT: So we put the secret key/keypair for the mint to GIVE CONSENT for the transaction
+                  //            Hindi basta-basta papayag.
+    .rpc();
     console.log("Your transaction signature", tx);
   });
 });
